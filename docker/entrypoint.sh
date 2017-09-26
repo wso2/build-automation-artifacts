dc992389a1ca7a9e1fa82d146aad68e556712a86
riyafa <riyafa.12@cse.mrt.ac.lk>
2017-09-26 11:33:50 +0530

    Change docker file for ecs cluster

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,4 +12,25 @@ chown -R jenkins:jenkins /home/jenkins
 chown -R jenkins:jenkins /build/software/maven
 
 chown -R jenkins:jenkins /build/gpg-keys/.gnupg
-exec "$@"
+
+
+if [[ $# -eq 1 ]]; then
+
+	# if `docker run` only has one arguments, we assume user is running alternate command like `bash` to inspect the image
+	exec "$@"
+
+else
+
+	# if -tunnel is not provided try env vars
+	if [[ "$@" != *"-tunnel "* ]]; then
+		if [[ ! -z "$JENKINS_TUNNEL" ]]; then
+			TUNNEL="-tunnel $JENKINS_TUNNEL"		
+		fi
+	fi
+
+	if [[ ! -z "$JENKINS_URL" ]]; then
+		URL="-url $JENKINS_URL"
+	fi
+
+	exec java $JAVA_OPTS -Duser.home=/home/jenkins -cp /usr/share/jenkins/slave.jar hudson.remoting.jnlp.Main -headless $TUNNEL $URL "$@"
+fi
